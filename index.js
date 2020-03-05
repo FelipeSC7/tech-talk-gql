b53c57a1b3f1fc5719effa4f0c3bec88efc8713d
Felipe Costa <felipecosta@sofist.com.br>
2020-03-05 13:27:55 -0300

    adjust package INIT project start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// const { ApolloServer, gql } = require('apollo-server')
-// const { importSchema } = require('graphql-import')
-// const resolvers = require('./resolvers')
+const { ApolloServer, gql } = require('apollo-server')
+const { importSchema } = require('graphql-import')
+const resolvers = require('./resolvers')
 
 
 const express = require('express')
@@ -14,11 +14,11 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// const schemaPath = './schema/index.graphql';
-// const server = new ApolloServer({
-//     typeDefs: importSchema(schemaPath),
-//     resolvers
-// })
+const schemaPath = './schema/index.graphql';
+const server = new ApolloServer({
+    typeDefs: importSchema(schemaPath),
+    resolvers
+})
 
 // server.listen().then(({ url }) => {
 //     console.log(`Executando em ${url}`)
